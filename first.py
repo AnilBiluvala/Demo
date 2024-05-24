ea11348ddeedb163512b0db2c07f66a24d7f08e5
unknown <anilbiluvala@gmail.com>
2024-05-24 14:39:40 +0530

    line number 3 is modified

--- first.py
+++ first.py
@@ -1,10 +1,4 @@
 print("First program")
 print("added second line")
 print("added third line")
-print("new feature added to form f1")
-print("added new feature to f5")
-print("added new f6")
-print("added new feature f7")
-print("new feature f8 is added")
-print("f9")
-print("f10")
+
