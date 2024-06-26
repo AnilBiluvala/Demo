6508ee166b1904383d038611e27ad94436048342
anilbiluvala <anilbiluvala@gmail.com>
2024-06-26 15:14:07 +0530

    new commit

--- first.py
+++ first.py
@@ -1,8 +1,4 @@
 print("First program")
 print("added second line")
-print("added third line")
-print("added fourth line")
-print("added fifth line")
-print("vvce is added");
-print("new line is added")
+
 
